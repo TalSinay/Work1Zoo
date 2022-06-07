965b3c6fc46e437a3701080e965811eb79a60caf
Talsinay <talsinay1000@gamil.com>
2022-06-08 00:24:27 +0300

    fix work 4

---
+++ src/graphics/Controller.java
@@ -0,0 +1,17 @@
+package graphics;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class Controller implements Observer {
+    private ZooPanel zoo;
+
+    public Controller(ZooPanel zoo){
+        zoo=zoo;
+    }
+
+
+    public void update(Observable o, Object arg) {
+        zoo.manageZoo();
+    }
+}
---
+++ src/graphics/graphics/CopyZoo.java
@@ -0,0 +1,24 @@
+package graphics.graphics;
+
+import animals.Animal;
+import graphics.*;
+
+import java.util.ArrayList;
+
+public class CopyZoo {
+    private ArrayList<Animal> animals = new ArrayList<Animal>();
+    private ArrayList<Object> foods = new ArrayList<Object>();
+    public ArrayList<Animal> getAnimals() {
+        ArrayList<Animal> animalss = new ArrayList<Animal>(animals);
+        return animalss;
+    }
+
+    public ArrayList<Object> getFoods() {
+        ArrayList<Object> foodss = new ArrayList<Object>(foods);
+        return foodss;
+    }
+    public CopyZoo(ZooPanel zp){
+//        animals = zp.getAnimals();
+//        foods = zp.getFoods();
+    }
+}
